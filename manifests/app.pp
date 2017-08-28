40d7ab4e8ef9fbb700792d8f969099ab6d7e4ec5
Philip Kirkbride <kirkins@gmail.com>
2017-08-28 00:24:06 -0400

    ensure latest from git, stop before starting npm

--- manifests/app.pp
+++ manifests/app.pp
@@ -1,12 +1,12 @@
 vcsrepo { '/home/pi/pop_machine':
-  ensure   => present,
+  ensure   => latest,
   provider => git,
   source   => 'https://github.com/kirkins/pop-machine-demo.git',
   revision => 'master',
 }
 
 exec { 'npm start':
-  command => "/usr/bin/npm start",
+  command => "/usr/bin/npm stop & /usr/bin/npm start",
   cwd     => "/home/pi/pop_machine/",
 }
 
